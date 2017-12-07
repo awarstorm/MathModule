0b6ad3057b19d3512ddbb29b47a9dda96f067214
Anthony Warstorm <awarstorm@vermillion.eaglecrk.local>
2017-12-07 14:12:36 -0600

    Initial commit

---
+++ src/com/eaglecrk/awarstorm/modules/AddingModule.java
@@ -0,0 +1,14 @@
+package com.eaglecrk.awarstorm.modules;
+
+public class AddingModule {
+
+	/**
+	 * Returns the sum of two integers
+	 * @param int a
+	 * @param int b
+	 * @return a+b
+	 */
+	public static int sum ( int a, int b) {
+		return a+b;
+	}
+}
---
+++ src/com/eaglecrk/awarstorm/modules/AddingModuleTest.java
@@ -0,0 +1,14 @@
+package com.eaglecrk.awarstorm.modules;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AddingModuleTest {
+
+	@Test
+	void testSum() {
+		fail("Not yet implemented");
+	}
+
+}
